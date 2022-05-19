f80a5415c2c20ba1d882a1feed341b836fb83682
M4x1m3 <M4x1me@protonmail.com>
2022-05-19 09:36:53 +0200

    [py/test] Renames random test to urandom

--- python/test/urandom.cpp
+++ python/test/urandom.cpp
@@ -4,7 +4,7 @@
 QUIZ_CASE(python_random) {
   TestExecutionEnvironment env = init_environement();
   assert_command_execution_fails(env, "random()");
-  assert_command_execution_succeeds(env, "from random import *");
+  assert_command_execution_succeeds(env, "from urandom import *");
   assert_command_execution_succeeds(env, "random()");
   assert_command_execution_succeeds(env, "getrandbits(23)");
   deinit_environment();
