374d5664fa29b996bdcae24962f937956cf22a2e
M4x1m3 <M4x1me@protonmail.com>
2022-05-21 11:29:17 +0200

    [apps/settings] Added progress bar on format popup (closes #2)

--- apps/settings/sub_menu/storage_controller.cpp
+++ apps/settings/sub_menu/storage_controller.cpp
@@ -1,6 +1,10 @@
 #include "storage_controller.h"
+#include "../../apps_container.h"
 #include <assert.h>
 #include <cmath>
+
+#include <ion/timing.h>
+#include <ion/keyboard.h>
 
 namespace Settings {
 
@@ -60,20 +64,130 @@ void StorageController::willDisplayCellForIndex(HighlightCell * cell, int index)
 }
 
 FormatPopUpController::FormatPopUpController() :
-  ::PopUpController(
-    4,
-    Invocation(
-      [](void * context, void * sender) {
-        Ion::FileSystem::format();
-        Container::activeApp()->dismissModalViewController(false);
-        return true;
-      }, this)
-  )
+  ::PopUpController(4, Invocation([](void * context, void * sender) { return true; }, this)),
+  m_contentView(this, 4, Invocation([](void * context, void * sender) {
+    Ion::FileSystem::format([](int current, int total, void* sndr) {
+      ((FormatPopUpController::ContentView*) ((HighContrastButton*) sndr)->parentResponder())->setProgress(current, total);
+      AppsContainer::sharedAppsContainer()->redrawWindow();
+      Ion::Keyboard::scan();
+    }, sender);
+
+    Container::activeApp()->dismissModalViewController(false);
+    return true;
+  }, this))
 {
   m_contentView.setMessage(0, I18n::Message::FormatLaunch1);
   m_contentView.setMessage(1, I18n::Message::FormatLaunch2);
   m_contentView.setMessage(2, I18n::Message::FormatLaunch3);
   m_contentView.setMessage(3, I18n::Message::FormatLaunch4);
+}
+
+View * FormatPopUpController::view() {
+  return &m_contentView;
+}
+
+void FormatPopUpController::didBecomeFirstResponder() {
+  m_contentView.setSelectedButton(0);
+}
+
+bool FormatPopUpController::handleEvent(Ion::Events::Event event) {
+  if (event == Ion::Events::Left && m_contentView.selectedButton() == 1) {
+    m_contentView.setSelectedButton(0);
+    return true;
+  }
+  if (event == Ion::Events::Right && m_contentView.selectedButton() == 0) {
+    m_contentView.setSelectedButton(1);
+    return true;
+  }
+  return false;
+}
+
+FormatPopUpController::ContentView::ContentView(Responder * parentResponder, int numberOfLines, Invocation okInvocation) :
+  Responder(parentResponder),
+  m_cancelButton(
+    this, I18n::Message::Cancel,
+    Invocation(
+      [](void * context, void * sender) {
+        Container::activeApp()->dismissModalViewController();
+        return true;
+      }, this),
+    KDFont::SmallFont),
+  m_okButton(this, I18n::Message::Ok, okInvocation, KDFont::SmallFont),
+  m_warningTextView(KDFont::SmallFont, I18n::Message::Warning, 0.5, 0.5, KDColorWhite, KDColorBlack),
+  m_numberOfLines(numberOfLines),
+  m_messageTextViews{}
+{
+  assert(m_numberOfLines <= k_maxNumberOfLines && m_numberOfLines >= 0);
+  for (int i = 0; i < m_numberOfLines; i++) {
+    m_messageTextViews[i].setFont(KDFont::SmallFont);
+    m_messageTextViews[i].setAlignment(0.5f, 0.5f);
+    m_messageTextViews[i].setBackgroundColor(KDColorBlack);
+    m_messageTextViews[i].setTextColor(KDColorWhite);
+  }
+}
+
+void FormatPopUpController::ContentView::setSelectedButton(int selectedButton) {
+  m_cancelButton.setHighlighted(selectedButton == 0);
+  m_okButton.setHighlighted(selectedButton == 1);
+  Container::activeApp()->setFirstResponder(selectedButton == 0 ? &m_cancelButton : &m_okButton);
+}
+
+int FormatPopUpController::ContentView::selectedButton() {
+  return m_cancelButton.isHighlighted() ? 0 : 1;
+}
+
+void FormatPopUpController::ContentView::setMessage(int index, I18n::Message message) {
+  assert(index >=0 && index < m_numberOfLines);
+  m_messageTextViews[index].setMessage(message);
+}
+
+int FormatPopUpController::ContentView::numberOfSubviews() const {
+  // MessageTextViews + WarningTextView + ProgressBarView + CancelButton + OkButton
+  return m_numberOfLines + 4;
+}
+
+View * FormatPopUpController::ContentView::subviewAtIndex(int index) {
+  int totalSubviews = numberOfSubviews();
+  if (index < 0 || index >= totalSubviews) {
+    assert(false);
+    return nullptr;
+  }
+  if (index == 0) {
+    return &m_warningTextView;
+  }
+  if (index == totalSubviews - 3) {
+    return &m_progressBarView;
+  }
+  if (index == totalSubviews - 2) {
+    return &m_cancelButton;
+  }
+  if (index == totalSubviews - 1) {
+    return &m_okButton;
+  }
+  return &m_messageTextViews[index-1];
+}
+
+void FormatPopUpController::ContentView::layoutSubviews(bool force) {
+  KDCoordinate height = bounds().height();
+  KDCoordinate width = bounds().width();
+  KDCoordinate textHeight = KDFont::SmallFont->glyphSize().height();
+  m_warningTextView.setFrame(KDRect(0, k_topMargin, width, textHeight), force);
+
+  // Offset to center text vertically
+  const int offset = (k_maxNumberOfLines - m_numberOfLines) / 2;
+
+  for (int i = 0; i < m_numberOfLines; i++) {
+    m_messageTextViews[i].setFrame(KDRect(0, k_topMargin + k_paragraphHeight + (i + 1 + offset) * textHeight, width, textHeight), force);
+  }
+
+  m_progressBarView.setFrame(KDRect(k_buttonMargin, height - k_buttonMargin - k_buttonHeight - k_progressbarMargin - k_progressbarHeight, width - 2 * k_buttonMargin, k_progressbarHeight), force);
+
+  m_cancelButton.setFrame(KDRect(k_buttonMargin, height - k_buttonMargin - k_buttonHeight, (width - 3 * k_buttonMargin) / 2, k_buttonHeight), force);
+  m_okButton.setFrame(KDRect(2 * k_buttonMargin + (width - 3 * k_buttonMargin) / 2, height - k_buttonMargin - k_buttonHeight, (width - 3 * k_buttonMargin) / 2, k_buttonHeight), force);
+}
+
+void FormatPopUpController::ContentView::setProgress(int current, int total) {
+  m_progressBarView.setValue(current, total);
 }
 
 }
--- apps/settings/sub_menu/storage_controller.h
+++ apps/settings/sub_menu/storage_controller.h
@@ -2,6 +2,7 @@
 #define SETTINGS_STORAGE_CONTROLLER_H
 
 #include <escher/pop_up_controller.h>
+#include <escher/progress_bar_view.h>
 
 #include "generic_sub_controller.h"
 #include "selectable_view_with_messages.h"
@@ -11,6 +12,37 @@ namespace Settings {
 class FormatPopUpController : public ::PopUpController {
 public:
   FormatPopUpController();
+  View * view() override;
+  void didBecomeFirstResponder() override;
+  bool handleEvent(Ion::Events::Event event) override;
+protected:
+  class ContentView : public View, public Responder {
+  public:
+    ContentView(Responder * parentResponder, int numberOfLines, Invocation okInvocation);
+    void drawRect(KDContext * ctx, KDRect rect) const override { ctx->fillRect(bounds(), KDColorBlack); }
+    void setSelectedButton(int selectedButton);
+    int selectedButton();
+    void setMessage(int index, I18n::Message message);
+    void setProgress(int current, int total);
+  private:
+    constexpr static KDCoordinate k_progressbarMargin = 10;
+    constexpr static KDCoordinate k_progressbarHeight = 4;
+    constexpr static KDCoordinate k_buttonMargin = 10;
+    constexpr static KDCoordinate k_buttonHeight = 20;
+    constexpr static KDCoordinate k_topMargin = 8;
+    constexpr static KDCoordinate k_paragraphHeight = 20;
+    int numberOfSubviews() const override;
+    View * subviewAtIndex(int index) override;
+    void layoutSubviews(bool force = false) override;
+    ProgressBarView m_progressBarView;
+    HighContrastButton m_cancelButton;
+    HighContrastButton m_okButton;
+    MessageTextView m_warningTextView;
+    const int m_numberOfLines;
+    constexpr static int k_maxNumberOfLines = 4;
+    MessageTextView m_messageTextViews[k_maxNumberOfLines];
+  };
+  ContentView m_contentView;
 };
 
 class StorageController : public GenericSubController {
